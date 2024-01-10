5aee93a7b3beb61f5037b66c74d6231bed88f7e4
ErikTromp <e.t.tromp@gmail.com>
2024-01-10 19:31:54 +0100

    should be string

--- translate.py
+++ translate.py
@@ -149,7 +149,7 @@ def main():
                         translated_texts = []
                         # Free some memory
                         gc.collect()
-                        if device.startswith('cuda'):
+                        if str(device).startswith('cuda'):
                             torch.cuda.empty_cache()
 
                 # Write checkpoint
@@ -159,7 +159,7 @@ def main():
 
             # One source language down, release the memory
             gc.collect()
-            if device.startswith('cuda'):
+            if str(device).startswith('cuda'):
                 torch.cuda.empty_cache()
 
 if __name__ == "__main__":
