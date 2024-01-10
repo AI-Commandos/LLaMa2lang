a161c2edd899b36936c1ed158b9bc5e080f625e7
Mirek Vink <info@mirekvink.nl>
2024-01-10 15:27:46 +0100

    Added a dpo thread creator

---
+++ create_thread_prompts_dpo.py
@@ -0,0 +1,123 @@
+from datasets import load_dataset, DatasetDict, Dataset, load_from_disk
+import sys
+import os
+import pandas as pd
+from tqdm import tqdm
+import argparse
+
+# Function to format a thread in LLaMa2 format
+def format_thread(thread, system_instruction):
+    formatted_thread = f"<s>[INST] <<SYS>>\n{system_instruction}\n<</SYS>>\n\n"
+
+    for i in range(0, len(thread), 2):
+        user_msg = thread[i]['text'] if i < len(thread) else ""
+        model_answer = thread[i+1]['text'] if i+1 < len(thread) else ""
+        formatted_thread += f"{user_msg} [/INST] {model_answer} </s>"
+        if i+2 < len(thread):
+            formatted_thread += f"<s>[INST] "
+
+    return formatted_thread
+
+def format_dpo(thread, system_instruction, bad_child):
+    formatted_thread = {
+        'prompt': f"<s>[INST] <<SYS>>\n{system_instruction}\n<</SYS>>\n\n"
+    }
+
+    for i in range(0, len(thread) - 2, 2):
+        user_msg = thread[i]['text'] if i < len(thread) else ""
+        model_answer = thread[i+1]['text'] if i+1 < len(thread) else ""
+        formatted_thread['prompt'] += f"{user_msg} [/INST] {model_answer} </s>"
+        if i+2 < len(thread):
+            formatted_thread['prompt'] += f"<s>[INST] "
+
+    formatted_thread['prompt'] += f"{thread[len(thread) - 2]['text']} [/INST]"
+    formatted_thread['chosen'] = f"{thread[len(thread) - 1]['text']} </s>"
+    formatted_thread['rejected'] = f"{bad_child[1]['text']} </s>"
+
+    return formatted_thread
+
+# We only continue the thread with the highest ranked answer to each input
+def find_children_and_highest_ranked_child(df, parent_id):
+    children = df[df['parent_id'] == parent_id]
+    max = children['rank'].max()
+
+    if not children.empty:
+      return children[children['rank'] == max], children[children['rank'] != max]
+    return None, None
+
+def main():
+    parser = argparse.ArgumentParser(description="Turn the translated dataset into threads in LLaMa2-chat format. We do this by always using the highest ranking answer following a given input prompt.")
+    parser.add_argument('dataset_name', type=str, 
+                        help='The input dataset, loaded from Huggingface datasets or disk. This should be the result of the previous step.')
+    parser.add_argument('instruction_prompt', type=str, 
+                        help='An instruction message added to every prompt given to the chatbot to force it to answer in the target language. Example: "You are a generic chatbot that always answers in English."')
+    parser.add_argument('output_location', type=str,
+                        help='Where to write the Huggingface Dataset to. Can be a disk location or a Huggingface Dataset repository. Be sure to set up HF_TOKEN.')
+    parser.add_argument('--base_dataset_text_field', type=str, default="text",
+                        help="The dataset's column name containing the actual text to translate. Defaults to text")
+    parser.add_argument('--base_dataset_rank_field', type=str, default="rank",
+                        help="The dataset's column name containing the rank of an answer given to a prompt. Defaults to rank")
+    parser.add_argument('--base_dataset_id_field', type=str, default="message_id",
+                        help="The dataset's column name containing the id of a text. Defaults to message_id")
+    parser.add_argument('--base_dataset_parent_field', type=str, default="parent_id",
+                        help="The dataset's column name containing the parent id of a text. Defaults to parent_id")
+    args = parser.parse_args()
+    dataset_name = args.dataset_name
+    instruction_prompt = args.instruction_prompt
+    output_location = args.output_location
+    base_dataset_text_field = args.base_dataset_text_field
+    base_dataset_rank_field = args.base_dataset_rank_field
+    base_dataset_id_field = args.base_dataset_id_field
+    base_dataset_parent_field = args.base_dataset_parent_field
+
+    if os.path.isdir(dataset_name):
+        dataset = load_from_disk(os.path.join(dataset_name))
+    else:
+        dataset = load_dataset(dataset_name)
+    
+    # Construct threads
+    folds = dataset.keys()
+    threads = {k: [] for k in folds}
+    for fold in folds:
+        print(f"Creating threads for fold {fold}")
+        
+        df = dataset[fold].to_pandas()
+
+        # Replace NULLs in rank with a value lower than the lowest rank
+        min_rank = df[base_dataset_rank_field].min()
+        df[base_dataset_rank_field].fillna(min_rank - 1, inplace=True)
+
+        # Identify root messages (those without a parent_id)
+        root_messages = df[df[base_dataset_parent_field].isna()]
+
+        with tqdm(total=len(root_messages)) as pbar:
+            for _, root_message in root_messages.iterrows():
+                # Create the thread
+                thread = [{'text': root_message[base_dataset_text_field]}]
+
+                good_child, bad_children = find_children_and_highest_ranked_child(df, root_message[base_dataset_id_field])
+
+                while good_child is not None and not good_child.empty:
+                    thread.append({'text': good_child.iloc[0][base_dataset_text_field]})
+
+                    for bad_child in bad_children.iterrows():
+                        threads[fold].append(format_dpo(thread, instruction_prompt, bad_child))
+
+                    good_child, bad_children = find_children_and_highest_ranked_child(df, good_child[base_dataset_id_field].iloc[0])
+
+                # Update progress
+                pbar.update(1)
+
+        threads[fold] = Dataset.from_pandas(pd.DataFrame(data=threads[fold]))
+
+    dataset = DatasetDict(threads)
+
+    # Check if output location is a valid directory
+    if os.path.isdir(output_location):
+        dataset.save_to_disk(output_location)
+    else:
+        # Try to push to hub, requires HF_TOKEN environment variable to be set, see https://huggingface.co/docs/huggingface_hub/package_reference/environment_variables#hftoken
+        dataset.push_to_hub(output_location)
+
+if __name__ == "__main__":
+    main()
