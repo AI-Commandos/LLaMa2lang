6fd11dff2e07c8634a530ae1af81fe6c2edcfb96
Mirek Vink <info@mirekvink.nl>
2024-02-06 13:19:46 +0100

    More dpo

--- create_thread_prompts_dpo.py
+++ create_thread_prompts_dpo.py
@@ -1,55 +1,55 @@
-from datasets import load_dataset, DatasetDict, Dataset, load_from_disk
-import sys
-import os
-import pandas as pd
-from tqdm import tqdm
 import argparse
+import os
 
-# Function to format a thread in LLaMa2 format
-def format_thread(thread, system_instruction):
-    formatted_thread = f"<s>[INST] <<SYS>>\n{system_instruction}\n<</SYS>>\n\n"
+import pandas as pd
+from datasets import load_dataset, DatasetDict, Dataset, load_from_disk
+from pandas import DataFrame
+from tqdm import tqdm
+from transformers import (
+    AutoTokenizer, PreTrainedTokenizerBase,
+)
 
-    for i in range(0, len(thread), 2):
-        user_msg = thread[i]['text'] if i < len(thread) else ""
-        model_answer = thread[i+1]['text'] if i+1 < len(thread) else ""
-        formatted_thread += f"{user_msg} [/INST] {model_answer} </s>"
-        if i+2 < len(thread):
-            formatted_thread += f"<s>[INST] "
+
+def format_dpo(thread: list[str], system_instruction: str, bad_child: str, tokenizer: PreTrainedTokenizerBase) \
+        -> dict[str, str]:
+    chat = [
+        {"role": "system", "content": system_instruction}
+    ]
+
+    formatted_thread: dict[str, str] = {}
+
+    for i in range(0, len(thread) - 1):
+        if i % 2 == 0:
+            chat.append({"role": "user", "content": thread[i]})
+        else:
+            chat.append({"role": "assistant", "content": thread[i]})
+
+    formatted_thread['prompt'] = tokenizer.apply_chat_template(chat, tokenize=False)[len(tokenizer.bos_token):]
+    formatted_thread['chosen'] = thread[len(thread) - 1]
+    formatted_thread['rejected'] = bad_child
 
     return formatted_thread
 
-def format_dpo(thread, system_instruction, bad_child):
-    formatted_thread = {
-        'prompt': f"[INST] <<SYS>>\n{system_instruction}\n<</SYS>>\n\n"
-    }
-
-    for i in range(0, len(thread) - 2, 2):
-        user_msg = thread[i]['text'] if i < len(thread) else ""
-        model_answer = thread[i+1]['text'] if i+1 < len(thread) else ""
-        formatted_thread['prompt'] += f"{user_msg} [/INST] {model_answer} </s>"
-        if i+2 < len(thread):
-            formatted_thread['prompt'] += f"<s>[INST] "
-
-    formatted_thread['prompt'] += f"{thread[len(thread) - 2]['text']} [/INST]"
-    formatted_thread['chosen'] = f"{thread[len(thread) - 1]['text']}"
-    formatted_thread['rejected'] = f"{bad_child[1]['text']}"
-
-    return formatted_thread
 
 # We only continue the thread with the highest ranked answer to each input
-def find_children_and_highest_ranked_child(df, parent_id):
+def find_children_and_highest_ranked_child(df: DataFrame, parent_id: int) -> tuple[DataFrame, DataFrame]:
     children = df[df['parent_id'] == parent_id]
-    max = children['rank'].max()
+    max_rank = children['rank'].max()
 
     if not children.empty:
-      return children[children['rank'] == max], children[children['rank'] != max]
-    return None, None
+        return children[children['rank'] == max_rank], children[children['rank'] != max_rank]
+
+    df_empty = children.iloc[:0, :].copy()
+
+    return df_empty, df_empty
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Turn the translated dataset into threads in LLaMa2-chat format. We do this by always using the highest ranking answer following a given input prompt.")
-    parser.add_argument('dataset_name', type=str, 
+    parser = argparse.ArgumentParser(
+        description="Turn the translated dataset into threads in LLaMa2-chat format. We do this by always using the highest ranking answer following a given input prompt.")
+    parser.add_argument('dataset_name', type=str,
                         help='The input dataset, loaded from Huggingface datasets or disk. This should be the result of the previous step.')
-    parser.add_argument('instruction_prompt', type=str, 
+    parser.add_argument('instruction_prompt', type=str,
                         help='An instruction message added to every prompt given to the chatbot to force it to answer in the target language. Example: "You are a generic chatbot that always answers in English."')
     parser.add_argument('output_location', type=str,
                         help='Where to write the Huggingface Dataset to. Can be a disk location or a Huggingface Dataset repository. Be sure to set up HF_TOKEN.')
@@ -61,6 +61,8 @@ def main():
                         help="The dataset's column name containing the id of a text. Defaults to message_id")
     parser.add_argument('--base_dataset_parent_field', type=str, default="parent_id",
                         help="The dataset's column name containing the parent id of a text. Defaults to parent_id")
+    parser.add_argument("--base_model", type=str, default="NousResearch/Llama-2-7b-chat-hf")
+
     args = parser.parse_args()
     dataset_name = args.dataset_name
     instruction_prompt = args.instruction_prompt
@@ -69,19 +71,23 @@ def main():
     base_dataset_rank_field = args.base_dataset_rank_field
     base_dataset_id_field = args.base_dataset_id_field
     base_dataset_parent_field = args.base_dataset_parent_field
+    base_model = args.base_model
+
+    # Load base tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(base_model, trust_remote_code=True)
 
     if os.path.isdir(dataset_name):
         dataset = load_from_disk(os.path.join(dataset_name))
     else:
         dataset = load_dataset(dataset_name)
-    
+
     # Construct threads
     folds = dataset.keys()
     threads = {k: [] for k in folds}
     for fold in folds:
         print(f"Creating threads for fold {fold}")
-        
-        df = dataset[fold].to_pandas()
+
+        df: DataFrame = dataset[fold].to_pandas()
 
         # Replace NULLs in rank with a value lower than the lowest rank
         min_rank = df[base_dataset_rank_field].min()
@@ -93,17 +99,20 @@ def main():
         with tqdm(total=len(root_messages)) as pbar:
             for _, root_message in root_messages.iterrows():
                 # Create the thread
-                thread = [{'text': root_message[base_dataset_text_field]}]
+                thread: list[str] = [root_message[base_dataset_text_field]]
 
-                good_child, bad_children = find_children_and_highest_ranked_child(df, root_message[base_dataset_id_field])
+                good_child, bad_children = find_children_and_highest_ranked_child(df,
+                                                                                  root_message[base_dataset_id_field])
 
-                while good_child is not None and not good_child.empty:
-                    thread.append({'text': good_child.iloc[0][base_dataset_text_field]})
+                while not good_child.empty:
+                    thread.append(good_child.iloc[0][base_dataset_text_field])
 
                     for bad_child in bad_children.iterrows():
-                        threads[fold].append(format_dpo(thread, instruction_prompt, bad_child))
+                        formatted_dpo = format_dpo(thread, instruction_prompt, bad_child[1]['text'], tokenizer)
+                        threads[fold].append(formatted_dpo)
 
-                    good_child, bad_children = find_children_and_highest_ranked_child(df, good_child[base_dataset_id_field].iloc[0])
+                    good_child, bad_children = find_children_and_highest_ranked_child(df, good_child[
+                        base_dataset_id_field].iloc[0])
 
                 # Update progress
                 pbar.update(1)
@@ -118,6 +127,7 @@ def main():
     else:
         # Try to push to hub, requires HF_TOKEN environment variable to be set, see https://huggingface.co/docs/huggingface_hub/package_reference/environment_variables#hftoken
         dataset.push_to_hub(output_location)
+
 
 if __name__ == "__main__":
     main()
