d51cff2802b25289b8beb7644c9317f41f8f14b0
ErikTromp <e.t.tromp@gmail.com>
2024-02-09 14:20:03 +0100

    Fix ref before assignment

--- translate.py
+++ translate.py
@@ -185,6 +185,7 @@ def translate_records(base_dataset_lang_field, base_dataset_text_field, batch_si
         f'[---- LLaMa2Lang ----] Got {len(records)} records for source language {source_lang}, skipping {last_checkpoint_n}, will process till {records_length}')
     pbar.total = records_length
     pbar.update(last_checkpoint_n)
+    last_cnt = 0
     for cnt in range(last_checkpoint_n, records_length, batch_size):
         # Translate a full batch
         batch = records[cnt:cnt + batch_size]
@@ -212,8 +213,9 @@ def translate_records(base_dataset_lang_field, base_dataset_text_field, batch_si
             gc.collect()
             if str(device).startswith('cuda'):
                 torch.cuda.empty_cache()
+        last_cnt = cnt
     # Write checkpoint
-    checkpoint_file = os.path.join(lang_checkpoint_location, f'upto_{cnt}.json')
+    checkpoint_file = os.path.join(lang_checkpoint_location, f'upto_{last_cnt}.json')
     with open(checkpoint_file, 'w', encoding='utf-8') as f:
         json.dump(batch, f)
 
